71c4eccfbe62e104bb6156cfaa6b4c4ea86283d2
Casserole1805 <156900553+Casserole1805@users.noreply.github.com>
2024-01-27 03:33:54 +1300

    Update drawMap.php

--- Tamriel-Diplo-Varient/Classes/drawMap.php
+++ Tamriel-Diplo-Varient/Classes/drawMap.php
@@ -2,35 +2,26 @@
 
 defined('IN_CODE') or die('This script can not be run by itself.');
 
-class ColdWarVariant_drawMap extends drawMap {
+class TamrielVariant_drawMap extends drawMap {
 
 	protected $countryColors = array(
 		0 => array(226, 198, 158), // Neutral
 		1 => array(160,   1,  31), // USSR
 		2 => array(1,    92, 135), // NATO
+		3 => array(160,   1,  31), // USSR
+		4 => array(1,    92, 135), // NATO
+		5 => array(160,   1,  31), // USSR
+		6 => array(1,    92, 135), // NATO
 	);
 
 	protected function resources() {
-		if( $this->smallmap )
-		{
 			return array(
-				'map'=>'variants/ColdWar/resources/smallmap.png',
-				'army'=>'contrib/smallarmy.png',
-				'fleet'=>'contrib/smallfleet.png',
-				'names'=>'variants/ColdWar/resources/smallmapNames.png',
-				'standoff'=>'images/icons/cross.png'
-			);
-		}
-		else
-		{
-			return array(
-				'map'=>'variants/ColdWar/resources/map.png',
+				'map'=>'variants/Tamriel-Diplo-Varient/Resources/territorymap.png',
 				'army'=>'contrib/army.png',
 				'fleet'=>'contrib/fleet.png',
-				'names'=>'variants/ColdWar/resources/mapNames.png',
+				'names'=>'variants/Tamriel-Diplo-Varient/Resources/territorynames.png',
 				'standoff'=>'images/icons/cross.png'
 			);
-		}
 	}
 }
 

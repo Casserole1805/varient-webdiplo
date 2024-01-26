d0e73d49a86d03ea64cd49b428319cb0807dabc6
Casserole1805 <156900553+Casserole1805@users.noreply.github.com>
2024-01-27 01:26:40 +1300

    Update varient.php

--- varient.php
+++ varient.php
@@ -5,35 +5,35 @@ defined('IN_CODE') or die('This script can not be run by itself.');
 class ColdWarVariant extends WDVariant {
 	public $id         = 91;
 	public $mapID      = 91;
-	public $name       = 'ColdWar';
-	public $fullName   = 'Cold War';
-	public $description= 'The best of vDips 2-player action brought to webDip!';
+	public $name       = 'Tamriel';
+	public $fullName   = 'Tamriel 4E 202';
+	public $description= 'New 6 player yr 202 Tamriel varient of Diplomacy!';
 	public $adapter    = 'Firehawk & Safari';
 	public $version    = '1';
 	public $codeVersion= '1.0.6';
 
-	public $countries=array('USSR','NATO');
+	public $countries=array('Morrowind','Cyrodiil','Highrock','Hammerfell','Aldmeri dominion','Argonia');
 
 	public function initialize() {
 		parent::initialize();
-		$this->supplyCenterTarget = 17;
+		$this->supplyCenterTarget = 16;
 	}
 
 	public function __construct() {
 		parent::__construct();
-		$this->variantClasses['drawMap']            = 'ColdWar';
-		$this->variantClasses['adjudicatorPreGame'] = 'ColdWar';
+		$this->variantClasses['drawMap']            = 'Tamriel';
+		$this->variantClasses['adjudicatorPreGame'] = 'Tamriel';
 	}
 
 	public function turnAsDate($turn) {
 		if ( $turn==-1 ) return "Pre-game";
-		else return ( $turn % 2 ? "Autumn, " : "Spring, " ).(floor($turn/2) + 1960);
+		else return ( $turn % 2 ? "Autumn, " : "Spring, " ).(floor($turn/2) + 202);
 	}
 
 	public function turnAsDateJS() {
 		return 'function(turn) {
 			if( turn==-1 ) return "Pre-game";
-			else return ( turn%2 ? "Autumn, " : "Spring, " )+(Math.floor(turn/2) + 1960);
+			else return ( turn%2 ? "Autumn, " : "Spring, " )+(Math.floor(turn/2) + 202);
 		};';
 	}
 }
